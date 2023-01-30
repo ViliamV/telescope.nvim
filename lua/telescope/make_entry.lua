96d7b2fce52f642b9524a7d9e93a96e85ae55a17
Viliam Valent <viliam@valent.email>
2023-01-30 15:09:36 +0100

    wip

--- lua/telescope/make_entry.lua
+++ lua/telescope/make_entry.lua
@@ -643,6 +643,7 @@ function make_entry.gen_from_buffer(opts)
     local changed = entry.info.changed == 1 and "+" or " "
     local indicator = entry.flag .. hidden .. readonly .. changed
     local lnum = 1
+    local col = vim.api.nvim_win_get_cursor(opts.winnr)[2]
 
     -- account for potentially stale lnum as getbufinfo might not be updated or from resuming buffers picker
     if entry.info.lnum ~= 0 then
@@ -663,6 +664,7 @@ function make_entry.gen_from_buffer(opts)
       bufnr = entry.bufnr,
       filename = bufname,
       lnum = lnum,
+      col = col,
       indicator = indicator,
     }, opts)
   end
